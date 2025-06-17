a44e158bbf5fa6d5a1dbf6f45df20cb88bbc1c79
Karlix <bloxyp@gmail.com>
2025-06-17 22:23:46 +0800

    trying out view post

---
+++ views/protected/view-page.php
@@ -0,0 +1,42 @@
+
+
+<section class="content">
+  <main class="main">
+    <section class="post-section">
+      <img src="../../php/design/photos/category.webp" alt="Main Post" class="main-image" />
+      <div class="post-content">
+        <h2 class="main-caption">Ang galing mo talaga wooo</h2>
+        <p class="author">Jameson Gelarman</p>
+        <div class="tags">#Technology</div>
+
+        <!-- Comments -->
+        <div class="comment-scroll">
+
+          <div class="comment-box">
+          <h3 class="comment-author">Lhanz Hubilla</h3>
+          <p class="comment-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          </div>
+
+          <div class="comment-box">
+            <h3 class="comment-author">Lhanz Hubilla</h3>
+            <p class="comment-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          </div>
+
+          <div class="comment-box">
+            <h3 class="comment-author">Lhanz Hubilla</h3>
+            <p class="comment-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          </div>
+
+          <div class="comment-box">
+            <h3 class="comment-author">Lhanz Hubilla</h3>
+            <p class="comment-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          </div>
+        </div>
+        <!-- Comment Input -->
+        <div class="comment-input-box">
+          <input type="text" placeholder="Comment here as Lhanz Hubilla" />
+        </div>
+      </div>
+    </section>
+  </main>
+</section>
